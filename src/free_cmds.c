0920bef0bdac04833d976f26cf484e884422d11e
bazaluga <bazaluga@student.42.fr>
2024-07-17 12:36:32 +0200

    feat: Add functions to easyly free cmds

---
+++ src/free_cmds.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_cmds.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 10:08:29 by bazaluga          #+#    #+#             */
+/*   Updated: 2024/07/17 12:16:39 by bazaluga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/quoicoushell.h"
+
+void	free_cmd(void *content)
+{
+	t_cmd	*cmd;
+
+	cmd = (t_cmd *)content;
+	free_split(cmd->cmd_opts);
+	free(cmd->redir_in);
+	free(cmd->redir_out);
+	free(cmd->lim_heredoc);
+	free(cmd);
+}
+
+void	free_cmds(t_lstcmds *cmds)
+{
+	ft_lstclear(&cmds->cmds, free_cmd);
+	free_split(cmds->env);
+	free_split(cmds->paths);
+}
