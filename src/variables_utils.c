2f5598dae295cc303e96aed50538cca5927e43de
bazaluga <bazaluga@student.42.fr>
2024-07-26 19:03:08 +0200

    feat: Add env builtin

--- src/variables_utils.c
+++ src/variables_utils.c
@@ -6,7 +6,7 @@
 /*   By: jsommet <jsommet@student.42.fr >           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 02:51:47 by jsommet           #+#    #+#             */
-/*   Updated: 2024/07/26 19:54:17 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/07/23 22:45:46 by jsommet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,10 @@ char	**split_env_entry(char *entry)
 
 	split = (char **) calloc(3, sizeof(char *));
 	i = 0;
-	while (entry[i] && entry[i] != '=')
+	while (entry[i] != '=')
 		i++;
 	split[0] = ft_substr(entry, 0, i);
-	if (!entry[i])
-		split[1] = NULL;
-	else
-		split[1] = ft_strdup(entry + (++i));
+	i++;
+	split[1] = ft_strdup(entry + i);
 	return (split);
 }
