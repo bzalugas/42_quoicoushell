f998cfbe970d913d52c4a270834ae6a8d4733d4d
bazaluga <bazaluga@student.42.fr>
2024-07-30 18:47:58 +0200

    feat: Update ft_exit behavior

--- src/ft_exit.c
+++ src/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 22:14:35 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/07/30 00:20:32 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/07/30 18:47:31 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,28 @@
 
 int	ft_exit(t_cmd *cmd, t_shell *sh)
 {
-	unsigned int	status;
 	size_t			i;
 
 	if (!cmd->argv[1])
 		exit_shell(sh, sh->exit_code, true);
 	if (cmd->argc > 2)
-		exit_shell(sh, 2, true);
+	{
+		ft_dprintf(STDERR_FILENO,
+			"exit\nminishell: exit: too many arguments\n");
+		return (1);
+	}
 	i = 0;
 	while (cmd->argv[1][i])
 	{
 		if (!ft_isdigit(cmd->argv[1][i]))
-			exit_shell(sh, 2, true);
+		{
+			ft_dprintf(STDERR_FILENO,
+				"exit\nminishell: exit: %s: numeric argument required\n",
+				cmd->argv[1]);
+			exit_shell(sh, 2, false);
+		}
 		i++;
 	}
-	status = ft_atou_base(cmd->argv[1], "0123456789");
-	exit_shell(sh, status % 256, true);
+	exit_shell(sh, ft_atou_base(cmd->argv[1], "0123456789") % 256, true);
 	return (0);
 }
