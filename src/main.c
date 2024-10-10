e8fa6b2cd4eeb72f7878b6bd17b21854e7f18e69
bazaluga <bazaluga@student.42.fr>
2024-10-10 13:06:28 +0200

    refactor: norm main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jsommet <jsommet@student.42.fr >           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:45:26 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/10/08 14:21:40 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/10/10 13:06:20 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	g_sig = 0;
 
 t_shell	*init_shell(char **envp)
 {
-	static t_shell sh = (t_shell){0};
+	static t_shell	sh = (t_shell){0};
 
 	if (!sh.cwd)
 	{
@@ -32,48 +32,40 @@ t_shell	*init_shell(char **envp)
 	return (&sh);
 }
 
-char *get_redir_type(t_redir_type type)
-{
-	switch (type)
-	{
-		case RTIN: return "IN";
-		case RTOUT_A: return "OUT_APPEND";
-		case RTOUT_T: return "OUT_TRUNC";
-	}
-	return (NULL);
-}
-
-void	command_line(t_shell *sh, char *line)
+static void	get_cmds(t_shell *sh, char *line, t_lstcmds *cmds)
 {
 	char		**tokens;
 	t_tokens	t;
 	t_cmd		*cmd;
-	t_lstcmds	cmds;
 
-	if (!line)
-		return ;
-	if (!check_syntax(line))
-	{
-		ft_putstr_fd("Syntax Error\n", 2);
-		return ;
-	}
-	cmds = (t_lstcmds){0, .fd[0][0]=-1, .fd[0][1]=-1, .fd[1][0]=-1,
-		.fd[1][1]=-1, 0};
 	t = (t_tokens){0};
 	while (t.start > -1)
 	{
 		tokens = token_split(line, &t);
 		if (!tokens)
 			exit_shell(sh, EXIT_FAILURE, false);
-// TODO: PROTECT MALLOC
-		cmd = get_command(sh, tokens, cmds.n_cmds);
-		cmds.n_cmds++;
+		cmd = get_command(sh, tokens, cmds->n_cmds);
+		cmds->n_cmds++;
 		if (!cmd)
 			exit_shell(sh, EXIT_FAILURE, false);
-// TODO: PROTECT MALLOC
-		ft_lstadd_back(&cmds.cmds, ft_lstnew(cmd));
+		ft_lstadd_back(&cmds->cmds, ft_lstnew(cmd));
 		free_split(tokens);
 	}
+}
+
+void	command_line(t_shell *sh, char *line)
+{
+	t_tokens	t;
+	t_lstcmds	cmds;
+
+	if (!line)
+		return ;
+	if (!check_syntax(line))
+		return (ft_putstr_fd("Syntax Error\n", 2));
+	cmds = (t_lstcmds){0, .fd[0][0] = -1, .fd[0][1] = -1, .fd[1][0] = -1,
+		.fd[1][1] = -1, 0};
+	t = (t_tokens){0};
+	get_cmds(sh, line, &cmds);
 	sh->cmds = &cmds;
 	run_all_cmds(&cmds, sh);
 	free_cmds(&cmds);
