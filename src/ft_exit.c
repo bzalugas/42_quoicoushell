c046b836337ca011740ac335c9c3e9f4d6baeda4
jsommet <jsommet@student.42.fr>
2024-10-16 22:37:24 +0200

    fix: exit behavior when there are whitespaces or a sign before the number

--- src/ft_exit.c
+++ src/ft_exit.c
@@ -6,29 +6,40 @@
 /*   By: jsommet <jsommet@student.42.fr >           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 22:14:35 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/10/16 20:24:28 by jsommet          ###   ########.fr       */
+/*   Updated: 2024/10/16 22:27:55 by jsommet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "quoicoushell.h"
 
+bool	is_numeric_argument(char *arg)
+{
+	size_t	i;
+
+	i = 0;
+	while (ft_isspace(arg[i]))
+		i++;
+	if (arg[i] == '-' || arg[i] == '+')
+		i++;
+	while (arg[i])
+	{
+		if (!ft_isdigit(arg[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 int	ft_exit(t_cmd *cmd, t_shell *sh)
 {
-	size_t			i;
-
 	if (!cmd->argv[1])
 		exit_shell(sh, sh->exit_code, true);
-	i = 0;
-	while (cmd->argv[1][i])
+	if (!is_numeric_argument(cmd->argv[1]))
 	{
-		if (!ft_isdigit(cmd->argv[1][i]))
-		{
-			ft_dprintf(STDERR_FILENO,
-				"exit\nminishell: exit: %s: numeric argument required\n",
-				cmd->argv[1]);
-			exit_shell(sh, 2, false);
-		}
-		i++;
+		ft_dprintf(STDERR_FILENO,
+			"exit\nminishell: exit: %s: numeric argument required\n",
+			cmd->argv[1]);
+		exit_shell(sh, 2, false);
 	}
 	if (cmd->argc > 2)
 	{
