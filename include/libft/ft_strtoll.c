846db072de718afa3f469d96a6004b3f71acccc0
bazaluga <bazaluga@student.42.fr>
2024-10-21 16:25:16 +0200

    feat: Finish strtoll

--- include/libft/ft_strtoll.c
+++ include/libft/ft_strtoll.c
@@ -6,55 +6,76 @@
 /*   By: jsommet <jsommet@student.42.fr >           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 18:56:05 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/10/18 18:56:58 by jsommet          ###   ########.fr       */
+/*   Updated: 2024/10/21 16:24:43 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ignore_start(const char *nptr, int *sign, int *base)
+static int	ignore_start(const char *nptr, int *sign, int *base, char **endptr)
 {
 	int	i;
 
+	if (endptr)
+		*endptr = (char *)nptr;
 	*sign = 1;
 	i = 0;
 	while (nptr[i] && ft_isspace(nptr[i]))
 		i++;
-	if (nptr[i] == '-' || (nptr[i] == '+' && i++))
+	if ((nptr[i] == '-' || nptr[i] == '+') && ++i)
 		*sign = ((nptr[i - 1] == '-') * -1) + ((nptr[i - 1] == '+') * 1);
 	if (*base == 0)
 	{
-		if (ft_strncmp(nptr, "0x", 2) && ft_strncmp(nptr, "0X", 2))
-			*base = 10;
+		if (!ft_strncmp(nptr, "0x", 2) || !ft_strncmp(nptr, "0X", 2))
+			*base = 16;
 		else if (nptr[0] == '0')
 			*base = 8;
+		else
+			*base = 10;
 	}
-	if (!ft_strncmp(nptr, "0x", 2) || !ft_strncmp(nptr, "0X", 2))
+	if ((!ft_strncmp(nptr, "0x", 2) || !ft_strncmp(nptr, "0X", 2))
+		&& *base == 16)
 		i += 2;
 	return (i);
 }
 
-static char get_base_rank(char c, int base)
+static char	get_base_rank(char c, int base)
 {
 	char	res;
 
 	res = -1;
 	if (ft_isdigit(c))
 		res = c - '0';
-	if (ft_isalpha(c) && c >= 'a' && c <= 'z')
+	else if (ft_isalpha(c) && c >= 'a' && c <= 'z')
 		res = 10 + (c - 'a');
-	if (ft_isalpha(c) && c >= 'A' && c <= 'Z')
+	else if (ft_isalpha(c) && c >= 'A' && c <= 'Z')
 		res = 10 + (c - 'A');
 	if (res >= base)
 		res = -1;
 	return (res);
 }
 
-static long long stop_fun(long long res, const char *end, char **endptr)
+static void	set_endptr(const char *nptr, char **endptr, int base)
 {
-	if (endptr)
-		*endptr = (char *)end;
-	return (res);
+	if (!endptr)
+		return ;
+	while (nptr && *nptr)
+	{
+		if (get_base_rank(*nptr, base) == -1)
+			break ;
+		nptr++;
+	}
+	if (nptr > *endptr + 1)
+		*endptr = (char *)nptr;
+}
+
+static long long	stop_fun(long long res, int sign)
+{
+	if (sign > 0 && res > LLONG_MAX / sign)
+		return (LLONG_MAX);
+	if (res != 0 && sign < 0 && sign < LLONG_MIN / res)
+		return (LLONG_MIN);
+	return (res * sign);
 }
 
 long long	ft_strtoll(const char *nptr, char **endptr, int base)
@@ -63,37 +84,43 @@ long long	ft_strtoll(const char *nptr, char **endptr, int base)
 	int			sign;
 	char		tmp;
 
-	if (!nptr || ((base != 0 && base < 2) || base > 36))
-	{
-		if (endptr)
+	if (endptr)
 			*endptr = NULL;
+	if (!nptr || ((base != 0 && base < 2) || base > 36))
 		return (0);
-	}
-	nptr += ignore_start(nptr, &sign, &base);
+	nptr += ignore_start(nptr, &sign, &base, endptr);
+	set_endptr(nptr, endptr, base);
 	res = 0;
-	while (nptr)
+	while (nptr && *nptr)
 	{
 		tmp = get_base_rank(nptr[0], base);
 		if (tmp == -1)
-			return (stop_fun(res, nptr, endptr));
+			return (stop_fun(res, sign));
+		if (sign > 0 && res * base + tmp < res)
+			return (stop_fun(LLONG_MAX, 1));
+		if (sign < 0 && res * base + tmp < res)
+			return (stop_fun(LLONG_MIN, -1));
 		res = res * base + tmp;
 		nptr++;
 	}
-	return (stop_fun(res, nptr, endptr));
+	return (stop_fun(res, sign));
 }
-#include <stdio.h>
-int main(int ac, char **av)
-{
-	(void)ac;
-	char *nb = av[1];
-	int base = atoi(av[2]);
-	char *end1 = NULL;
-	char *end2 = NULL;
-	long long res1;
-	long long res2;
-//need to fix 09AZB 0
-	res1 = strtoll(nb, &end1, base);
-	printf("res1 = %lld, end1 = <%s>\n", res1, end1);
-	res2 = ft_strtoll(nb, &end2, base);
-	printf("res2 = %lld, end2 = <%s>\n", res2, end2);
-}
+/* #include <stdio.h> */
+/* int main(int ac, char **av) */
+/* { */
+/* 	(void)ac; */
+/* 	char *nb = av[1]; */
+/* 	int base = atoi(av[2]); */
+/* 	char *end1 = NULL; */
+/* 	char *end2 = NULL; */
+/* 	long long res1; */
+/* 	long long res2; */
+/* 	/\* nb = "-1fldsflj4x7az0"; *\/ */
+/* 	/\* nb = "9223372036854775807"; *\/ */
+/* 	nb = "-9,223372036854775808808.sdf"; */
+/* //need to fix 09AZB 0 */
+/* 	res1 = strtoll(nb, &end1, base); */
+/* 	printf("res1 = %lld, end1 = <%s>\n", res1, end1); */
+/* 	res2 = ft_strtoll(nb, &end2, base); */
+/* 	printf("res2 = %lld, end2 = <%s>\n", res2, end2); */
+/* } */
