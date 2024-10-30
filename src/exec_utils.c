097e2dc7e394a6266a09868404e85c07c8bf7e8f
bazaluga <bazaluga@student.42.fr>
2024-10-30 18:18:11 +0100

    fix: uninitialized value when single command

--- src/exec_utils.c
+++ src/exec_utils.c
@@ -6,7 +6,7 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 10:15:15 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/10/30 17:56:08 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/10/30 18:17:29 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,14 @@ bool	wait_all(pid_t last_pid, int *last_status)
 	int		status;
 	bool	signal;
 
+	status = 0;
 	signal = false;
 	if (last_pid != -1)
 		waitpid(last_pid, last_status, 0);
 	while (errno != ECHILD)
 	{
 		wait(&status);
-		if (WIFSIGNALED(status)
+		if (errno != ECHILD && WIFSIGNALED(status)
 			&& (WTERMSIG(status) == SIGINT || WTERMSIG(status) == SIGQUIT))
 			signal = true;
 	}
